fd42915dda6a9781f8c79e0d2923c13ef8d53e3c
nguyenle510 <trieunguyen30@gmail.com>
2017-09-29 16:34:55 +0700

    Bai1

---
+++ Bai1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();
+            Console.WriteLine(hostName);
+            // Lấy IP
+            string IP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+            Console.WriteLine("IP Address :" + IP);
+            Console.ReadKey();
+        }
+    }
+}
